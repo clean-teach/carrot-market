4122f70cebcefeb1d386c1a049ee5ff54fde80a6
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-12-27 22:53:44 +0900

    #12.4 Answer
    ## 이전 강의 추가
    
    - `useMutation()` 의 mutate 요청에서 문제가 생기지 않기 위하여,
    
        → loading 중 일 경우에는 요청을 보내지 않는다. (이전 요청이 완료 된 후에만 요청을 보냄)
    
        - 빠른 클릭 등으로 **경쟁 상태(race condition)** 이 발생 할 수 있다.
            - **경쟁 상태(race condition)** : 삭제와 생성 요청이 순차적으로 이루어지지 않고, 동시에 요청 되어서 이미 삭제 된 것을 또 삭제하라는 등의 명령이 발생 할 수 있다.
    - [ ]  “궁금해요” 버튼 요청을 보내기 전에(댓글도 마찬가지), 백엔드에서는 먼저 소속 된 해당 글이 존재하는지 먼저 확인 하여야 한다.
        - 존재 하지 않는다면, 404 에러 등을 반환 하여야 한다.
        - 사용자를 신뢰해서는 안 되기 때문
    
    ## 목표
    
    - react-hook-form과 Mutation 을 활용해서 답변 기능을 구현하여 본다.
        - 댓글이 제출 완료 되었다면, 댓글 입력창을 비운다.

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -7,6 +7,8 @@ import useSWR from 'swr';
 import Link from 'next/link';
 import useMutation from '@libs/client/useMutation';
 import { cls } from '@libs/client/utils';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
 
 interface AnswerWithUser extends Answer {
   user: User;
@@ -27,12 +29,23 @@ interface CommunityPostResponse {
   isWondering: boolean
 }
 
+interface AnswerForm {
+  answer: string;
+}
+
+interface AnswerResponse {
+  ok: boolean;
+  answer: Answer;
+}
+
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter();
+  const { register, handleSubmit, reset } = useForm<AnswerForm>();
   const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null
   );
-  const [wonder] = useMutation(`/api/posts/${router.query.id}/wonder`);
+  const [wonder, { loading: wonderLoading }] = useMutation(`/api/posts/${router.query.id}/wonder`);
+  const [sendAnswer, { data: answerData, loading: answerLoading }] = useMutation<AnswerResponse>(`/api/posts/${router.query.id}/answers`);
   const onWonderClick = () => {
     if (!data) return;
     mutate({
@@ -50,8 +63,20 @@ const CommunityPostDetail: NextPage = () => {
     },
       false
     );
-    wonder({});
+    if (!wonderLoading) {
+      wonder({});
+    }
   };
+  const onValid = (form: AnswerForm) => {
+    if (answerLoading) return;
+    sendAnswer(form);
+  };
+
+  useEffect(() => {
+    if (answerData && answerData.ok) {
+      reset();
+    }
+  }, [answerData, reset])
 
   return (
     <Layout canGoBack>
@@ -131,14 +156,17 @@ const CommunityPostDetail: NextPage = () => {
           ))}
         </div>
         <div className="px-4">
-          <TextArea
-            name="description"
-            placeholder="Answer this question!"
-            required
-          />
-          <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-            Reply
-          </button>
+          <form onSubmit={handleSubmit(onValid)}>
+            <TextArea
+              register={register('answer', { required: true, minLength: 5 })}
+              name="description"
+              placeholder="Answer this question!"
+              required
+            />
+            <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
+              {answerLoading ? 'Loading...' : 'Reply'}
+            </button>
+          </form>
         </div>
       </div>
     </Layout>
