cc0831ad02c8d754de5a3e17a7d7880fc8689699
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-01-09 00:14:51 +0900

    #13.5 Edit Profile part One
    ## 목표
    
    - “Edit Profile” 페이지를 만들어 본다.
        - 프로필 ‘수정’ 페이지이기 때문에, 기존의 유저 정보를 form에서 보여 주어야 한다.
        - 회원의 종류는 “email” 방식과 “phone” 방식이 있기 때문에, 두 개의 경우에 맞게 보호를 해 주어야 한다.
            - 둘 다 입력하지 않았을 때는 에러를 발생 시켜야 하므로, react-hook-form 의 `setError()` 와 `formErrors`, `formState: {errors}` 를 사용해서 에러를 만들어 보자.
    
    ## 추가
    
    - 회원 전용 페이지를 보호하기 위해서, [기존에 만들었던 `useUser()` Hook](https://www.notion.so/10-AUTHORIZATION-3397f68d62304716aa0e489942ed78a7?pvs=21) 을 회원 전용페이지에서 호출 한다.
    
    ### 챌린지
    
    - [ ]  현재 회원 전용 페이지가 아닌 페이지는 `enter.tsx` 페이지 밖에 없기 때문에 `useUser()` Hook을 `pages/_app.tsx` 에서 사용 하도록 수정 해 본다.
        - 힌트 :
            - `useUser()` Hook의 인자로 회원 전용이 아닌 페이지를 받는다.

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,9 +1,15 @@
+import { User } from '@prisma/client';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import useSWR from 'swr';
 
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
+
 export default function useUser() {
-  const { data, error } = useSWR('/api/users/me');
+  const { data, error } = useSWR<ProfileResponse>('/api/users/me');
   const router = useRouter();
 
   useEffect(() => {
@@ -11,5 +17,6 @@ export default function useUser() {
       router.replace('/enter');
     }
   }, [data, router]);
+
   return { user: data?.profile, isLoading: !data && !error };
 }
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,11 +2,31 @@ import type { NextPage } from 'next';
 import Button from '@components/button';
 import Input from '@components/input';
 import Layout from '@components/layout';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import useUser from '@libs/client/useUser';
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const { register, handleSubmit, setValue, setError, formState: { errors } } = useForm<EditProfileForm>();
+  const onValid = (form: EditProfileForm) => {
+    if (form.email == '' && form.phone == '') { setError('formErrors', { message: 'Email or Phone number are required. You need to Choose one.' }) }
+  }
+
+  useEffect(() => {
+    if (user?.email) { setValue('email', user.email) }
+    if (user?.phone) { setValue('phone', user.phone) }
+  }, [user, setValue])
+
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
           <div className="w-14 h-14 rounded-full bg-slate-500" />
           <label
@@ -22,14 +42,15 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
-        <Input
-          required
+        <Input register={register('email')} required={false} label="Email address" name="email" type="email" />
+        <Input register={register('phone')}
+          required={false}
           label="Phone number"
           name="phone"
-          type="number"
+          type="text"
           kind="phone"
         />
+        {errors.formErrors ? <p className=' my-2 text-red-500 font-medium'>{errors.formErrors.message}</p> : null}
         <Button text="Update profile" />
       </form>
     </Layout>
