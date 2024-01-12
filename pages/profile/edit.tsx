7931d592303b328425d6a9e0862ff606a4fcaea9
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-01-12 21:18:27 +0900

    #15.5 Cloudflare Upload
    ## 목표
    
    - 응답 받은 ****Cloudflare**** 의 비어있는 URL에 프론트엔드에서 이미지 파일을 업로드 하여 본다.
        - 일회성 업로드 URL에 사용 할 Form을 자바스크립트로 만들어 본다.
    - `FormData()` 에 대해서 알아본다.
    
    ## [Direct Creator Upload](https://developers.cloudflare.com/images/cloudflare-images/upload-images/direct-creator-upload/)
    
    - 다음은 일회성 업로드 URL을 사용하여, 사용자가 선택한 이미지를 업로드하는 간단한 HTML 페이지의 예입니다.
        - ****Cloudflare**** 에서 응답 받은 업로드용 URL은 일회용이고, 30분 뒤에 삭제 되기 때문에, 저장 하지 않는다.
    
    ```tsx
    < form
    action="INSERT_UPLOAD_URL_HERE"
    method="post"
    enctype="multipart/form-data"
    >
    	< input type="file" id="myFile" name="file" />
    	< input type="submit" />
    < /form>
    ```
    
    ## 참고
    
    ### `[FormData](https://developer.mozilla.org/ko/docs/Web/API/FormData)`
    
    - Ajax로 form 전송을 가능하게 해주는 `FormData` 객체를 생성
    - Ajax는 submit시 기본 폼 동작이 `e.preventDefault()`이기 때문에, 페이지 전환 없이 폼 데이터를 전송할 수 있습니다.
    - ex) `const FormData=new FormData()`로 `FormData`객체 생성
    - `FormData.append()`를 통해 key, value형태로 값을 추가
        - form에 일종의 input데이터를 추가한다는 의미
        - `FormData` 객체 안에 이미 키가 존재하면 그 키에 새로운 값을 추가하고, 키가 없으면 추가합니다.
        - ex) `formData.append('username','pizza');`
    - 보통 form태그나 input태그없이 Form을 이용해 데이터를 전송하려고 할 때 `FormData`를 사용
    
    ## 정리
    
    - 이번 강의에서는 ****Cloudflare**** 에서 응답 받은 이미지 업로드 용 URL에 이미지를 담아서 ****Cloudflare**** 에 업로드 하여 보았다.
        - 이미지를 업로드 하기 위하여,
            1. `FormData` 를 사용하여 자바스크립트로 form 을 만들고
            2. 이를 body에 담아서 POST 요청을 보냈다.

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -39,12 +39,16 @@ const EditProfile: NextPage = () => {
     if (email == '' && phone == '' && name == '') {
       return setError('formErrors', { message: 'Email or Phone number are required. You need to Choose one.' })
     }
-    if (avatar && avatar.length > 0) {
-      const cloudflareRequest = await (await fetch(`/api/files`)).json();
+    if (avatar && avatar.length > 0 && user) {
+      const { id, uploadURL } = await (await fetch(`/api/files`)).json();
+      const form = new FormData();
 
-      console.log(cloudflareRequest);
+      form.append("file", avatar[0], user?.id + "");
 
-      // upload file to CF URL
+      await fetch(uploadURL, {
+        method: "POST",
+        body: form,
+      });
 
       return;
       editProfile({
