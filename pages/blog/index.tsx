cafb4e8d2b8e7fa4396cfef8607454b1447ff8a4
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-01-29 22:04:11 +0900

    #19.11 getStaticPaths part One
    ## 목표
    
    - NextJS의 정적(Static) 페이지에서 URL 변수를 구현 하는 방법을 알아본다.
        - URL 변수 : 쿼리 URL, 파라미터 URL 과 같이 `[id].tsx` 형식으로 생성하는 페이지 라우트
        - 이전 강의에서 정적(Static) HTML 코드로 변환하기 위해 프로젝트 폴더 안에 생성한 `.md` 파일들들을 각각의 HTML 경로로 생성 되게 할 것이다.
    
    ## 과정
    
    1. `pages/` 디렉토리 안의 원하는 경로(`pages/blog/`)에
        1. `index.tsx` 파일 생성
            - 이전 강의에서 만들었던, `blog.tsx` 파일의 역할
            - `getStaticProps` 과 node.js(`readdireSync('경로')`, `readFileSync('파일 이름', 인코딩 형식)`) 등을 이용하여, 프로젝트 내의 정적 파일 (`.md`) 에 접근한다.
        2. `[id].tsx` 파일 생성
    2. `index.tsx` 에서 각각의 파라미터 페이지로 이동하기 위해 Link 버튼과 경로를 작성
        - 예 : ``blog/${id}``
        - 정적 페이지의 파라미터 id는 프로젝트 폴더에 미리 만들어진 파일들에 접근 하여야 하므로, `getStaticProps` 에서 node.js(`readdireSync('경로')`, `readFileSync('파일 이름', 인코딩 형식)`)  로 가져왔던, 파일 이름을 축출하여 props로 페이지 컴포넌트에 넘겨준다.
            - 파일 이름에서 확장자는 제외하고 파일 이름 자체만 가져오기 위하여, 자바스크립트 `.split()` 메소드를 사용해서 문자열을 분해
    
    ## 정리
    
    ### 주의 할 점
    
    - 동적(Dynamic) 페이지와 달리, 정적(Static) 페이지에서 만든 변수 페이지 (`[id].tsx`) 는 빌드 시점에서 변수의 갯수를 미리 알아야 한다.
        - 실시간으로 DB에 확인하여, 존재하지 않는 변수를 확인 할 수 있는 동적 페이지와는 달리,
        - 빌드 시에 한번만 생성되는 정적 페이지에서는 NextJS가 생성 될 페이지의 갯수를 미리 알아야 하기 때문
    
    ## 참고
    
    ### 미들웨어 (`_middleware` 사용)
    
    - 중간에 request를 가로챔
    
    ### Server (`getInitialProps` 또는 `getServerSideProps` 사용)
    
    - 런타임 시 서버 사이드 렌더링
    
    ### Static (초기 props를 사용하지 않음)
    
    - static HTML로 자동으로 렌더링됨
    
    ### SSG (`getStaticProps` 사용)
    
    - static HTML + JSON으로 자동으로 생성됨

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,11 +2,13 @@ import Layout from "@components/layout";
 import { readFileSync, readdirSync } from "fs";
 import matter from "gray-matter";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
     title: string;
     date: string;
     category: string;
+    slug: string;
 }
 
 const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
@@ -15,12 +17,16 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
             <h1 className="font-semibold text-center text-xl mt-5 mb-10">Latest Posts:</h1>
             {posts?.map((post, index) => (
                 <div key={index} className="mb-5">
-                    <span className="text-lg text-red-500">{post.title}</span>
-                    <div>
-                        <span>
-                            {post.date} / {post.category}
-                        </span>
-                    </div>
+                    <Link href={`/blog/${post.slug}`}>
+                        <a>
+                            <span className="text-lg text-red-500">{post.title}</span>
+                            <div>
+                                <span>
+                                    {post.date} / {post.category}
+                                </span>
+                            </div>
+                        </a>
+                    </Link>
                 </div>
             ))}
         </Layout>
@@ -31,7 +37,8 @@ export async function getStaticProps() {
     // export async function getServerSideProps() {
     const blogPosts = readdirSync('./posts').map(file => {
         const content = readFileSync(`./posts/${file}`, 'utf-8');
-        return matter(content).data;
+        const [slug, _] = file.split('.');
+        return { ...matter(content).data, slug };
     });
     return {
         props: {
