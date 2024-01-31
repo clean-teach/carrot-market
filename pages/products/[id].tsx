8a2cbe55257b2a62ba5e96a3b3b7fb13dc21f520
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-02-01 02:13:59 +0900

    #20.4 Blocking SSG
    ## 목표
    
    - 커뮤니티 페이지 같은 곳에서 사용 할 수 있는 ISR (Incremental Static Regeneration: 증분 정적 재생성)의 유용성을 알아본다.
    - URL 안에 변수가 있는 경로(`[id].tsx`)는 어떻게 페이지를 revalidate 하는지 알아본다.
        - Incremental Static Regeneration 과 Static Generation(정적 생성)을, “API에서 제공하는 Id를 활용하는 페이지”에서 어떻게 적용하는지 알아본다.
            - 파일 시스템과 `getStaticPaths` 를 바탕으로 만든 변수 페이지의 경우, pre-generate (미리 생성)하는 방법을 안다.
            - 하지만, API를 통해서 가져오는 id로 만든 변수 페이지의 경우, pre-generate 하려면 어떻게 하여야 할까?
    
                → `getStaticPaths`  의 `fallback` 을 활용 하여 위 문제를 해결해 볼 것이다.
    
    - Revalidation 과 `getStaticPaths` 를 어떻게 합칠지도 배워본다.
    
    ## 정리
    
    1. API에서 받아온 id로 만든 변수 페이지는, 시스템 파일을 사용하여 만든 변수 페이지처럼 빌드 시에 모든 페이지를 생성 하기에 부적절 하다.
        - API에서 받은 응답으로 얼마나 많은 변수를 만들어야 할지 알 수 없고,
        - 엄청나게 많은 페이지를 만들어야 할 경우,
            - DB 비용 증가 등
            - 빌드 시에 한번에 그 많은 정적 페이지를 생성 하는 것은 비 효율적이기 때문이다.
    2. `fallback: 'blocking'` 을 사용하면 API에서 받아온 데이터를 사용해서도 변수 페이지를 정적 생성 할 수 있다.
        - `npm run build` 시에는
    
            → 정적 페이지가 생성 되지 않는다.
    
        - `npm run start` 시
    
            → 비로소 접근한 페이지에 한하여, 정적 페이지가 하나 생성되어 있다.
    
            → 초기 한 번은 다운로드 시간이 걸리지만, 이후 새로고침을 했을 때에는 다운로드 없이 즉각 정적 화면을 확인 할 수 있다.
    
        - 어떻게 이게 가능 할까 ?? ([다음 강의](https://www.notion.so/20-INCREMENTAL-SITE-REGENERATION-a04cf7d10e36478f8f0df15789a45767?pvs=21)에서 계속..)
    
    ## 참고
    
    ### `fallback: 'blocking'`
    
    [Functions: getStaticPaths](https://nextjs.org/docs/pages/api-reference/functions/get-static-paths#fallback-blocking)
    
    - `fallback`이 `'blocking'`인 경우 `getStaticPaths`에서 반환되지 않은 새 경로는 SSR과 동일하게 HTML이 생성될 때까지 기다렸다가 이후 요청을 위해 캐시되어 path당 한 번만 발생합니다.
    - `fallback: 'blocking'`은 기본적으로 생성된 페이지를 업데이트하지 않습니다.
        - 생성된 페이지를 업데이트하려면 `fallback: blocking`과 함께 ISR을 사용하십시오.

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next';
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Button from '@components/button';
 import Layout from '@components/layout';
 import { useRouter } from 'next/router';
@@ -9,6 +9,7 @@ import useMutation from '@libs/client/useMutation';
 import { cls } from '@libs/client/utils';
 import Image from 'next/image';
 import { useEffect } from 'react';
+import client from "@libs/server/client";
 
 interface ProductWithUser extends Product {
   user: User;
@@ -21,7 +22,11 @@ interface ItemDetailResponse {
   isLiked: boolean;
 }
 
-const ItemDetail: NextPage = () => {
+const ItemDetail: NextPage<ItemDetailResponse> = ({
+  product,
+  relatedProducts,
+  isLiked,
+}) => {
   const router = useRouter();
   const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
@@ -41,23 +46,23 @@ const ItemDetail: NextPage = () => {
       <div className="px-4  py-4">
         <div className="mb-8">
           <div className="relative pb-80">
-            {data?.product.image ? <Image
-              src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${data?.product.image}/public`}
+            {product.image ? <Image
+              src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${product.image}/public`}
               className="bg-slate-300 object-cover"
               layout="fill"
             /> : <div className="bg-slate-300 object-cover" />
             }
           </div>
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
-            {data?.product?.user?.avatar ? <Image
+            {product?.user?.avatar ? <Image
               width={48}
               height={48}
-              src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${data?.product?.user?.avatar}/avatar`}
+              src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${product?.user?.avatar}/avatar`}
               className="w-12 h-12 rounded-full bg-slate-300"
             /> : <div className="w-12 h-12 rounded-full bg-slate-300" />}
             <div>
-              <p className="text-sm font-medium text-gray-700">{data?.product?.user?.name}</p>
-              <Link href={`/users/profiles/${data?.product?.user?.id}`}>
+              <p className="text-sm font-medium text-gray-700">{product?.user?.name}</p>
+              <Link href={`/users/profiles/${product?.user?.id}`}>
                 <a className="text-xs font-medium text-gray-500">
                   View profile &rarr;
                 </a>
@@ -65,18 +70,18 @@ const ItemDetail: NextPage = () => {
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">{data?.product?.name}</h1>
-            <span className="text-2xl block mt-3 text-gray-900">{data?.product?.price}</span>
+            <h1 className="text-3xl font-bold text-gray-900">{product?.name}</h1>
+            <span className="text-2xl block mt-3 text-gray-900">{product?.price}</span>
             <p className=" my-6 text-gray-700">
-              {data?.product?.description}
+              {product?.description}
             </p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="Talk to seller" />
-              <button onClick={onFavClick} className={cls("p-3 rounded-md flex items-center justify-center  hover:bg-gray-100 ", data?.isLiked ? 'text-red-400 hover:text-red-500' : 'text-gray-400 hover:text-gray-500')}>
+              <button onClick={onFavClick} className={cls("p-3 rounded-md flex items-center justify-center  hover:bg-gray-100 ", isLiked ? 'text-red-400 hover:text-red-500' : 'text-gray-400 hover:text-gray-500')}>
                 <svg
                   className="h-6 w-6 "
                   xmlns="http://www.w3.org/2000/svg"
-                  fill={data?.isLiked ? "currentColor" : "none"}
+                  fill={isLiked ? "currentColor" : "none"}
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                   aria-hidden="true"
@@ -109,6 +114,61 @@ const ItemDetail: NextPage = () => {
       </div>
     </Layout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  if (!ctx?.params?.id) {
+    return {
+      props: {},
+    };
+  }
+  const product = await client.product.findUnique({
+    where: {
+      id: +ctx.params.id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  const terms = product?.name.split(" ").map((word) => ({
+    name: {
+      contains: word,
+    },
+  }));
+  const relatedProducts = await client.product.findMany({
+    where: {
+      OR: terms,
+      AND: {
+        id: {
+          not: product?.id,
+        },
+      },
+    },
+  });
+  const isLiked = false;
+
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+      relatedProducts: JSON.parse(JSON.stringify(relatedProducts)),
+      isLiked,
+    },
+  };
 };
 
 export default ItemDetail;
